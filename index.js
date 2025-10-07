6b7911d52288736e0971522c6720c29ed957fdf4
Niels <ngraveron@datasolution.fr>
2025-10-07 10:39:01 +0200

    [add] routes and pug template

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ app.use(express.json());
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = '';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/', (req, resp) => {
 
-// * Code for Route 1 goes here
+})
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+app.get('/update-cars', (req, resp) => {
 
-// * Code for Route 2 goes here
+})
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
+app.post('/update-cars', (req, resp) => {
+    
+})
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
